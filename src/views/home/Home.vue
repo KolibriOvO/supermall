143cef8735764cc35c812ac77b7de358f2a3548a
KolibirOvO <vigilanteovo@gmail.com>
2023-06-14 19:48:54 +0800

    day2

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,40 @@
-<script setup>
+<script>
+import NavBar from "@/common/navbar/NavBar.vue";
+import {getHomeMultidata} from "@/network/home";
 
+export default {
+  name: 'Home',
+  components: {
+    NavBar
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: []
+    }
+  },
+  created() {
+    getHomeMultidata().then(res => {
+      this.banners = res.data.banners.list;
+      this.recommends = res.data.recommends.list
+    })
+  }
+}
 </script>
 
 <template>
-<div>
-  <h2>首页</h2>
-</div>
+  <div id="home">
+    <NavBar class="home-nav">
+      <template v-slot:center>
+        <div>购物街</div>
+      </template>
+    </NavBar>
+  </div>
 </template>
 
 <style scoped>
-
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+}
 </style>
