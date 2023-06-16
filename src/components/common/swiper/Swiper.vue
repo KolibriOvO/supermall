e82994c6d9b4cae224433451a32ae57b1fa82fa1
KolibirOvO <vigilanteovo@gmail.com>
2023-06-16 09:35:15 +0800

    轮播图

--- src/components/common/swiper/Swiper.vue
+++ src/components/common/swiper/Swiper.vue
@@ -1,11 +1,37 @@
-<script setup>
+<script>
+import Swiper from "swiper"
+import 'swiper/css/swiper.min.css';
 
+export default {
+  name: "Swiper",
+  mounted() {
+    new Swiper('.swiper-container', {
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: false //允许分页点击跳转
+      },
+      loop: true,
+      autoplay: {
+        delay: 1500,
+        disableOnInteraction: false
+      }
+    })
+  }
+}
 </script>
 
 <template>
-
+  <div class="swiper-container">
+    <div class="swiper-wrapper">
+      <slot name="swiper-item"></slot>
+    </div>
+    <div class="swiper-pagination"></div>
+  </div>
 </template>
 
 <style scoped>
-
+.swiper-container >>> .swiper-pagination-bullet-active {
+  opacity: 1;
+  background: red;
+}
 </style>
---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,39 @@
+<script>
+import Swiper from "@/components/common/swiper/Swiper.vue";
+
+export default {
+  name: 'HomeSwiper',
+  props: {
+    banners: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+  components:{
+    Swiper
+  }
+}
+</script>
+
+<template>
+  <swiper v-if="banners.length>1" class="swiper">
+    <template v-slot:swiper-item>
+      <div v-for="(item,index) in banners" class="swiper-slide" :key=index>
+        <a :href="item.link">
+          <img :src="item.image" alt="">
+        </a>
+      </div>
+    </template>
+  </swiper>
+</template>
+
+<style scoped>
+.swiper{
+  width: 100%;
+}
+.swiper img{
+  width: 100%;
+}
+</style>
