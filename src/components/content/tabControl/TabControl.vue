fcf9d7210514beff999cba508b9a5c6db40a5e87
KolibirOvO <vigilanteovo@gmail.com>
2023-06-17 09:00:40 +0800

    商品切换

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,58 @@
+<script>
+export default {
+  name: 'TabControl',
+  props: {
+    titles: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+  data() {
+    return {
+      currentIndex: 0
+    }
+  },
+  methods: {
+    itemClick() {
+      this.currentIndex = index;
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="tab-control">
+    <div class="tab-control-item"
+         v-for="(item,index) in titles"
+         :key="index"
+         :class="{active:index===currentIndex}"
+         @click="itemClick(index)">
+      <span>{{ item }}</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.tab-control {
+  display: flex;
+  text-align: center;
+  height: 40px;
+  line-height: 40px;
+  font-size: 15px;
+  background-color: #fff;
+}
+
+.tab-control-item {
+  flex: 1;
+}
+
+.active {
+  color: var(--color-high-text)
+}
+
+.active span {
+  border-bottom: 3px solid var(--color-tint);
+}
+</style>
