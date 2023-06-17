e525a34e0633cf33a371a4b5ee01215611be77ea
KolibirOvO <vigilanteovo@gmail.com>
2023-06-17 23:22:53 +0800

    回到顶部

---
+++ src/components/content/backTop/BackTop.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  name: 'BackTop',
+  methods:{
+    backClick(){
+      document.documentElement.scrollTo({left:0,top:0,behavior:'smooth'});
+      }
+    }
+}
+</script>
+
+<template>
+  <div class="back-top"
+       @click="backClick">
+    <img src="@/assets/img/common/top.png" alt="">
+  </div>
+</template>
+
+<style scoped>
+.back-top {
+  position: fixed;
+  right: 8px;
+  bottom: 55px;
+}
+
+.back-top img {
+  width: 43px;
+  height: 43px;
+}
+</style>
