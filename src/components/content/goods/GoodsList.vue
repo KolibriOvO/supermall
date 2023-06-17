85781f4eba544cc8fde03ccf6e9265f23402c644
KolibirOvO <vigilanteovo@gmail.com>
2023-06-17 09:00:11 +0800

    商品列表

---
+++ src/components/content/goods/GoodsList.vue
@@ -0,0 +1,32 @@
+<script>
+import GoodsListItem from "@/components/content/goods/GoodsListItem.vue";
+
+export default {
+  name: 'GoodList',
+  components: {GoodsListItem},
+  props: {
+    goods: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="goods">
+    <GoodsListItem v-for="(item,index) in goods"
+                   :key="index"
+                   :goods-item="item"/>
+  </div>
+</template>
+
+<style scoped>
+.goods {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+</style>
