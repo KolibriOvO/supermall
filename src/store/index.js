53103cddd0f59122f10d76ba49750cfcb892bdfe
KolibirOvO <vigilanteovo@gmail.com>
2023-06-25 20:02:02 +0800

    vuex-store 添加购物车

--- src/store/index.js
+++ src/store/index.js
@@ -5,13 +5,27 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    cartList: []
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
+    addCounter(state, payload) {
+      payload.count++
+    },
+    addToCart(state, payload) {
+      state.cartList.push(payload)
+    }
   },
   actions: {
+    addCart(context, payload) {
+      let oldProduct = context.state.cartList.find(item => item.iid === payload.iid)
+      if (oldProduct) {
+        context.commit('addCounter', oldProduct)
+      } else {
+        payload.count = 1
+        context.commit('addToCart', payload)
+      }
+    }
   },
-  modules: {
-  }
+  modules: {}
 })
