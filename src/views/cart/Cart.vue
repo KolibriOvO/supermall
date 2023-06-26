89dd88a7587868c11a32b6ae31726defcfd65b4d
KolibirOvO <vigilanteovo@gmail.com>
2023-06-26 20:18:52 +0800

    购物车

--- src/views/cart/Cart.vue
+++ src/views/cart/Cart.vue
@@ -1,11 +1,36 @@
-<script setup>
+<script>
+import NavBar from "@/components/common/navbar/NavBar.vue";
+import {mapGetters} from "vuex";
 
+export default {
+  name: 'Cart',
+  components: {
+    NavBar
+  },
+  computed: {
+    ...mapGetters({
+      length: 'cartLength',
+      list: 'carList'
+    })
+  }
+}
 </script>
 
 <template>
-
+  <div class="cart">
+    <NavBar class="nav-bar">
+      <template v-slot:center>购物车{{ length }}</template>
+    </NavBar>
+  </div>
 </template>
 
 <style scoped>
+.nav-bar {
+  background-color: var(--color-tint);
+  color: #fff;
+}
 
+.cart {
+  height: 100vh;
+}
 </style>
