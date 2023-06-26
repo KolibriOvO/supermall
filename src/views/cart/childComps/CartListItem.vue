d94b633f227e3e9a43f9ebfd1cf84d85579ba676
KolibirOvO <vigilanteovo@gmail.com>
2023-06-26 20:19:25 +0800

    购物车-列表物品

---
+++ src/views/cart/childComps/CartListItem.vue
@@ -0,0 +1,44 @@
+<script>
+import CheckedButton from "@/views/cart/childComps/CheckButton.vue";
+
+export default {
+  name: 'CartListItem',
+  components: {CheckedButton},
+  props: {
+    product: {
+      type: Object,
+      default() {
+        return {}
+      }
+    }
+  },
+  methods: {
+    checkClick() {
+      this.product.checked = !this.product.checked
+    }
+  }
+}
+</script>
+
+<template>
+  <div id="shop-item">
+    <div class="item-selector">
+      <CheckedButton @click.native="checkClick" :is-checked="product.checked"/>
+    </div>
+    <div class="item-img">
+      <img :src="product.image" alt="商品图片">
+    </div>
+    <div class="item-info">
+      <div class="item-title">{{ product.title }}</div>
+      <div class="item-desc">{{ product.desc }}</div>
+      <div class="info-bottom">
+        <div class="item-price left">¥{{ product.price }}</div>
+        <div class="item-count right">x{{ product.count }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
