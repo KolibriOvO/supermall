bec0f395aec5d811c35485f2dd8676b74378507a
KolibirOvO <vigilanteovo@gmail.com>
2023-06-26 20:19:20 +0800

    购物车-列表

---
+++ src/views/cart/childComps/CartList.vue
@@ -0,0 +1,35 @@
+<script>
+import CartListItem from "@/views/cart/childComps/CartListItem.vue";
+import {mapGetters} from "vuex";
+
+export default {
+  name: 'CartList',
+  components: CartListItem,
+  computed: {
+    ...mapGetters([
+      'carList'
+    ])
+  }
+}
+</script>
+
+<template>
+  <div class="cart-list">
+    <div class="content" ref="scroll">
+      <CartListItem v-for="(item,index) in cartList"
+                    :product="item"
+                    :key="index"/>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.cart-list {
+  height: calc(100% - 44px);
+}
+
+.content {
+  height: calc(100% - 95px);
+  overflow: hidden;
+}
+</style>
