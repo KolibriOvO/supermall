526aa0592a3c8c071cdb41b6e58f51a87042b2db
KolibirOvO <vigilanteovo@gmail.com>
2023-06-26 20:19:40 +0800

    购物车-选择按钮

---
+++ src/views/cart/childComps/CheckButton.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  name: 'CheckedButton',
+  props: {
+    isChecked: {
+      type: Boolean,
+      default: true
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <div class="check-button" :class="{'check':isChecked}">
+      <img src="@/assets/img/cart/tick.svg" alt="">
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.check-button {
+  border-radius: 50%;
+  border: 2px solid #aaa;
+}
+
+.check {
+  border-color: red;
+  background-color: red;
+}
+</style>
