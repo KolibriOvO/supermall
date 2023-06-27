7636ed91bd215fcd355e889da0c62168664042e2
KolibirOvO <vigilanteovo@gmail.com>
2023-06-27 19:38:26 +0800

    addCart添加Promise

--- src/store/index.js
+++ src/store/index.js
@@ -11,7 +11,7 @@ export default new Vuex.Store({
     cartLength(state) {
       return state.cartList.length
     },
-    carList(state) {
+    cartList(state) {
       return state.cartList
     }
   },
@@ -26,13 +26,17 @@ export default new Vuex.Store({
   },
   actions: {
     addCart(context, payload) {
-      let oldProduct = context.state.cartList.find(item => item.iid === payload.iid)
-      if (oldProduct) {
-        context.commit('addCounter', oldProduct)
-      } else {
-        payload.count = 1
-        context.commit('addToCart', payload)
-      }
+      return new Promise((resolve,reject)=>{
+        let oldProduct = context.state.cartList.find(item => item.iid === payload.iid)
+        if (oldProduct) {
+          context.commit('addCounter', oldProduct)
+          resolve('当前的商品数量+1')
+        } else {
+          payload.count = 1
+          context.commit('addToCart', payload)
+          resolve('添加新的商品')
+        }
+      })
     }
   },
   modules: {}
