e0b4a78979299c7938307ff9cf07e50ef127096f
KolibirOvO <vigilanteovo@gmail.com>
2023-06-28 21:03:31 +0800

    添加-FastClick，VueLazyLoad，Toast

--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import FastClick from 'fastclick'
+import VueLazyload from "vue-lazyload";
+import Toast from "@/components/common/toast";
 
 Vue.config.productionTip = false
+Vue.use(Toast)
+FastClick.attach(document.body)
+Vue.use(VueLazyload,{
+  loading:require('@/assets/img/common/placeholder.png')
+})
 
 new Vue({
   router,
